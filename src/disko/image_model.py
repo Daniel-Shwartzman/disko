97a5d4671f3656dae15f0c71cff8cf63b2b322d8
Gogs <gogs@fake.local>
2024-03-25 13:32:12 -0400

    create the model methods

--- src/disko/image_model.py
+++ src/disko/image_model.py
@@ -1,12 +1,20 @@
 # model.py
 from src.disko.sqlite import SQLiteCRUD
+from src.mvc.controller import ImageController
 
 class ImageDataModel:
     def __init__(self, db_file):
         self.db = SQLiteCRUD(db_file)
+        self.controller = ImageController(db_file)
 
-    def insert_images_with_amount(self, table_name, images):
-        
+    def insert_images_with_amount(self, table_name):
+        images = self.db.select_column(table_name, 'image_name')
+        amount = self.controller.calculate_amount_per_registry(images)
+        for registry, count in amount.items():
+            self.db.insert_data(table_name, (registry, count))
 
-    def insert_cluster_names_to_db(self, cluster_names):
-        
+    def insert_cluster_names_to_db(self):
+        cluster_names = self.controller.get_kubernetes_clusters()
+        for cluster_name in cluster_names:
+            self.db.insert_data('clusters', (cluster_name,))
+
