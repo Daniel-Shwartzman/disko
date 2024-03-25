33d6251f387cc4f76ff10738e26fa5e11c9ba07b
Gogs <gogs@fake.local>
2024-03-25 12:46:10 -0400

    create controller structure

---
+++ src/disko/image_controller.py
@@ -0,0 +1,21 @@
+# controller.py
+from kubernetes import config
+from model import ImageDataModel
+
+class ImageController:
+    def __init__(self, db_file):
+        self.model = ImageDataModel(db_file)
+
+    def get_kubernetes_clusters(self):
+
+    def is_from_dockerhub(self, image):
+
+    def calculate_amount_per_registry(self, images):
+
+    def calculate_percentages(self, table_name):
+
+    def transfer_image(self, image, new_registry, tag, username, password):
+
+    def copy_images(self, images, new_registry, tag, username, password):
+
+    def export_sha256(self, image_name, image_tag):
