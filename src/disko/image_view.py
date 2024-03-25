65613330729591ca22575ca678e9db04aabc3c1a
Gogs <gogs@fake.local>
2024-03-25 12:44:55 -0400

    create the view structure

---
+++ src/disko/image_view.py
@@ -0,0 +1,29 @@
+# view.py
+import tkinter as tk
+from tkinter import ttk, messagebox
+import time
+from src.mvc.controller import ImageController
+
+class ImageRegistryManager:
+    def __init__(self, db_file):
+        
+
+    def display_image_data(self, table_name):
+
+    def cluster_selection(self, clusters):
+
+    def confirm_cluster_selection(self, selected_cluster, cluster_selection_window):
+
+    def create_images_table_screen(self):
+
+    def update_columns(self):
+    
+    def select_docker_images(self):
+
+    def confirm_image_selection(self, selected_indices, select_images_window):
+    
+    def registry_input_screen(self, selected_images):
+
+    def submit_registry_details(self, pull_username, pull_password, push_username, push_password, push_url, push_tag, selected_images, window):
+
+    def run(self):
